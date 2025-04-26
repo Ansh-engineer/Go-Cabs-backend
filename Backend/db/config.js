435b3c870193290aee47a98130177b3be03e4e9a
Ansh Chauhan <127720310+Ansh-engineer@users.noreply.github.com>
2025-04-26 12:27:58 +0530

    Update config.js

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017');
+    const conn = await mongoose.connect('mongodb+srv://ansh132be22:9qwinMcMHKbbcYI7@cluster0.xbc6svj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log("DB Connected");
   } catch (err) {
     // console.error(Error: ${err.message});
